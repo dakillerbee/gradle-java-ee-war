3b0cc8b568bdb894bce0c87703e82123c1e953c4
Tobi Tiggers <dakillerbee@gmail.com>
2018-08-03 23:48:53 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gradle-java-ee-war
---
+++ src/main/java/ch/automated/web/HelloServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.automated.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author wit
+ */
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1) 
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");  
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        String name = request.getParameter("name");
+        if (name == null) name = "World";
+        request.setAttribute("user", name);
+        request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+}
